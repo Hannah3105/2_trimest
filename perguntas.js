fb9f480d508c7a8909f88efb8845ce31ccf480ff
Hannah3105 <alana.justino.santos@escola.pr.gov.br>
2025-06-12 07:55:15 -0300

    12

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,31 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'história',
+    'A primeira capital do Brasil foi:?',
+    'Salvador'
 )
 
 criaCartao(
     'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Qual a capital dos Estados Unidos?',
+    'Washington'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'ciências',
+    'Qual o nome do processo pelo qual as plantas produzem alimento??',
+    'fotossintese'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
-)
+    'Lógica',
+    'Alguns meses têm 30 dias, outros 31. Quantos têm 28 dias??',
+    'todos os meses, pois todos tem pelo menos 28 dias'
+)Pedro está desenvolvendo o seu projeto e percebeu que sempre que ele adiciona um novo cartão, ele aparece na mesma linha do anterior e o espaço inferior da página nunca é utilizado. Além disso, os cartões estão muito próximos uns aos outros, dificultando a navegação na página.
+
+Pensando nisso, ordene todos os blocos abaixo que adicionam a propriedade necessária para fazer uma quebra de linha nos cartões e incluir um espaçamento de 3rem entre cada cartão.
+
+￼wrap;
+￼#container {
+￼gap:
+￼flex-wrap:
+￼3rem;}
