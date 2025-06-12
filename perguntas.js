96bfcd7c77235af4bb431ece70edc0367e8da2af
Hannah3105 <alana.justino.santos@escola.pr.gov.br>
2025-06-12 07:57:58 -0300

    12.05

--- perguntas.js
+++ perguntas.js
@@ -20,12 +20,4 @@ criaCartao(
     'Lógica',
     'Alguns meses têm 30 dias, outros 31. Quantos têm 28 dias??',
     'todos os meses, pois todos tem pelo menos 28 dias'
-)Pedro está desenvolvendo o seu projeto e percebeu que sempre que ele adiciona um novo cartão, ele aparece na mesma linha do anterior e o espaço inferior da página nunca é utilizado. Além disso, os cartões estão muito próximos uns aos outros, dificultando a navegação na página.
-
-Pensando nisso, ordene todos os blocos abaixo que adicionam a propriedade necessária para fazer uma quebra de linha nos cartões e incluir um espaçamento de 3rem entre cada cartão.
-
-￼wrap;
-￼#container {
-￼gap:
-￼flex-wrap:
-￼3rem;}
+)
